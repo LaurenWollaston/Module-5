4851eb5068d3ae894e06168d3029382cdbe31570
Lauren Wollaston <laurenwollaston@outlook.com>
2023-03-14 18:15:34 -0500

    finished project

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-function savetostorage(){
-  console.log(this.innerHTML);
-}
-var userdata = [];
-const svbtn = document.querySelectorAll('.saveBtn');
+$(window).bind("load", function () {
+const svbtn = document.querySelector('.buttonwrapper');
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  svbtn.addEventListener("click", function(event){
-    console.log("TEST");
+ svbtn.addEventListener("click", (event) =>{
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+  var maniphour = event.target.id;
+  console.log(maniphour);
+  localStorage.setItem(maniphour,document.getElementById("txt"+maniphour).value);
   });
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 var crntime=dayjs().format('H');
-console.log(crntime);
 
 for (i=9;i<18;i++){
   if (i<crntime){
@@ -53,7 +33,6 @@ for (i=9;i<18;i++){
   };
 }
 
-
 getday();
 setInterval(getday,1000);
 function getday(){
@@ -61,4 +40,13 @@ function getday(){
   var date=dayjs().format('dddd, MMMM Do');
   document.getElementById('currentDay').innerHTML = date;
 };
-console.log(userdata);
+
+function initformdata(){
+  for(i=9;i<18;i++){
+    var curhourval = localStorage.getItem(i);
+    document.getElementById("txt"+i).value=curhourval;
+  }
+}
+
+initformdata();
+});
